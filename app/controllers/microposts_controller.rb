c16b56eeb148280a565b21f860c85e60a057da1e
Kosuke Matsueda <coqoon.matsu@gmail.com>
2017-12-03 05:28:25 +0000

    20171203

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -4,7 +4,7 @@ class MicropostsController < ApplicationController
   def create
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
-      flash[:success] = "Micropost created!"
+      flash[:success] = "ツイートを作成しました！"
       redirect_to root_url
     else
       render 'static_pages/home'
@@ -15,7 +15,7 @@ class MicropostsController < ApplicationController
     @micropost = current_user.microposts.find_by(id: params[:id])
     return redirect_to root_url if @micropost.nil?
     @micropost.destroy
-    flash[:success] = "Micropost deleted"
+    flash[:success] = "ツイートを削除しました"
     redirect_to request.referrer || root_url
   end
   
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+# before_action :correct_user only:[:edit, :update, :delete]
+
 
   def show
     @user = User.find(params[:id])
@@ -12,7 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = "ようこそ the Sample App へ！!"
       redirect_to @user
     else
       render 'new'
@@ -21,24 +23,45 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    unless @user.id == session[:user_id] then
+      flash[:danger] = "不正な入力です"
+      redirect_to current_user
+    end
   end
   
   def update
+    
     @user = User.find(params[:id])
-    if @user.update_attributes(user_params)
-      flash[:success] = "情報を更新しました"
-      redirect_to @user
+
+    if @user.id == current_user.id
+      if (!@user.authenticate(params[:user][:cur_password]))
+        flash[:danger] = '現在のパスワードに誤りがあります'
+        render 'edit'
+      elsif @user.update_attributes(user_params)
+        flash[:success] = "情報を更新しました"
+        redirect_to current_user
+      else
+        # フォームのエラーメッセージと重複するので、メッセージは割愛
+        render 'edit'
+      end
     else
-      render 'edit'
+      flash[:danger] = "不正な入力です"
+      redirect_to @user
     end
   end
-
 
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :locate, :profile,
                                  :password_confirmation)
+  end
+  
+  def correct_user
+    @user = User.find(params[:id])
+    if (current_user != @user)
+      redirect_to root_url
+    end
   end
   
 end
